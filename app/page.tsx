c9aba853b0d238ef08d46e4333dbcaffa3eba8dc
Nick Olson <naolson.1819@gmail.com>
2025-02-15 14:10:55 -0600

    removed junk carousels

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-
-// import CardCarousel from '../components/card-carousel';
-// import QuoteCarousel from '../components/card-carousel-2';
 import { Container } from '../components/container';
-import CardCarousel from '../components/daisy-carousel';
 
 export default async function Home() {
   return (
     <Container>
-      <CardCarousel />
+      Hello
     </Container>
   );
 }
